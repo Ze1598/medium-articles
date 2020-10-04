ff4894aaf147b9fca84243604a435e8f69b4d35e
José Fernando Costa <jose.fernando.costa.1998@gmail.com>
2020-10-04 21:15:52 +0100

    Added "Using RegEx to extract expenses information from a text file"

---
+++ Using RegEx to extract expenses information from a text file/price_regex.py
@@ -0,0 +1,27 @@
+import re
+import datetime
+import pandas as pd
+
+PATTERN = r'((^([\d]+)([\.]?)([\d]+))|^([\d]+))( - )(.*)'
+
+# Load text
+with open("expenses.txt", "r") as f:
+	expenses_txt = f.readlines()
+# Put all the lines into a single string
+whole_txt = "".join(expenses_txt)
+
+# Find all the expense matches
+matches = re.findall(PATTERN, whole_txt, flags=re.MULTILINE)
+# Extract the relevant match information
+expenses = [ [m[7], m[0]] for m in matches ]
+
+# Create a DF for the expenses
+df = pd.DataFrame(data=expenses)
+# Reset the index so we have an actual column for it
+df.reset_index(inplace=True)
+# Rename the columns
+df.columns = ["ExpenseID", "Name", "Cost"]
+# Increase all IDs so they start at 1
+df["ExpenseID"] += 1
+# Export it as a CSV
+df.to_csv("expenses.csv", index=False)
